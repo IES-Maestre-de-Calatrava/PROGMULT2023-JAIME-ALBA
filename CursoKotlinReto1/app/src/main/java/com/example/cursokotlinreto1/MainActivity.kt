f08c7224cfbb57a66cc7e67dcc3650bb713a696e
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2023-10-06 14:30:54 +0200

    06/10/2023
    Reto 1 del curso de Kotlin.

---
+++ CursoKotlinReto1/app/src/main/java/com/example/cursokotlinreto1/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.cursokotlinreto1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+val TAG:String = ":::::::::::::"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var jaime:Programador = Programador()
+
+        Log.d(TAG,jaime.getProgrammerData().name)
+        Log.d(TAG,jaime.getProgrammerData().age.toString())
+        Log.d(TAG,jaime.getProgrammerData().language)
+    }
+}
+
+public class Programador() : ProgramadorInterface {
+    override fun getProgrammerData():ProgrammerData {
+        var datos:ProgrammerData = ProgrammerData(returnName(), returnAge(), returnLanguage())
+        return datos;
+    }
+
+    private fun returnName():String {
+        var name:String = "Jaime"
+        return name
+    }
+
+    private fun returnAge():Int {
+        var age:Int = 28
+        return age
+    }
+
+    private fun returnLanguage():String {
+        var language:String = "Kotlin"
+        return language;
+    }
+}
+
+
+interface ProgramadorInterface {
+    fun getProgrammerData():ProgrammerData
+}
+
+public data class ProgrammerData (
+    val name:String,
+    val age:Int,
+    val language:String
+)
