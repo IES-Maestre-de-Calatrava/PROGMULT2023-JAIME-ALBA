588e04aa878823c4e560d87d50901eacb0dc1ce3
jaimealba <jaimealba@outlook.es>
2023-10-15 23:35:31 +0200

    15/10/2023
    Creación de mi app personal, Monster Hunter Finder. Creación del layout para el menú principal. Inicio de la creación del layout para el perfil de usuario.

---
+++ MonsterHunterFinder/app/src/main/java/com/example/monsterhunterfinder/activity_buscador_palabras.kt
@@ -0,0 +1,11 @@
+package com.example.monsterhunterfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_buscador_palabras : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buscador_palabras)
+    }
+}
---
+++ MonsterHunterFinder/app/src/main/java/com/example/monsterhunterfinder/activity_mensajes_general.kt
@@ -0,0 +1,11 @@
+package com.example.monsterhunterfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_mensajes_general : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mensajes_general)
+    }
+}
