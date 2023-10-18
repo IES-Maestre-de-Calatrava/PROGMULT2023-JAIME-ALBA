dd1098cfad30478c0b60b9a8275d59f11cb629fc
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2023-10-18 11:06:11 +0200

    18/10/2023
    Proyecto completo del menú contextual, funcional y con el ejercicio que ha mandado Javier.
    Proyecto completo de las notificaciones, con el ejercicio que ha mandado Javier sobre crear dos botones y que cada uno llame a una notificación (no está lo de una sola notificación con dos posibles textos diferentes)

--- MenuContextual20232024/app/src/main/java/com/example/menucontextual20232024/MainActivity.kt
+++ MenuContextual20232024/app/src/main/java/com/example/menucontextual20232024/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.menucontextual20232024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
 import com.example.menucontextual20232024.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +13,66 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        crearObjetosDelXml()
+
+        registerForContextMenu(binding.editTextIdioma)
+        // Ejercicio: conseguir hacer lo mismo para el EditText de Oficio
+        registerForContextMenu(binding.editTextOficio)
+    }
+
+
+    // Paso como parámetros el ContextMenu que voy a usar y la view en la que voy a hacer
+    // la pulsación larga
+    override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        when (v){
+            binding.editTextIdioma -> menuInflater.inflate(R.menu.menu_idiomas, menu)
+            binding.editTextOficio -> menuInflater.inflate(R.menu.menu_oficio, menu)
+        }
 
     }
+
+
+    // Función para que ajuste un texto en el EditText según la opción seleccionada
+    // en el menú contextual
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.ingles -> {
+                binding.editTextIdioma.setText("Ingles")
+                true
+            }
+
+            R.id.frances -> {
+                binding.editTextIdioma.setText("Traidor a la sangre")
+                true
+            }
+
+            R.id.aleman -> {
+                binding.editTextIdioma.setText("Alemán")
+                true
+            }
+
+            R.id.informatica -> {
+                binding.editTextOficio.setText("Pobre desgraciado")
+                true
+            }
+
+            R.id.comercial -> {
+                binding.editTextOficio.setText("Comercial")
+                true
+            }
+
+            R.id.administrativo -> {
+                binding.editTextOficio.setText("Administrativo")
+                true
+            }
+
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+
+
 
     private fun crearObjetosDelXml() {
         binding = ActivityMainBinding.inflate(layoutInflater)
---
+++ Notificaciones/app/src/main/java/com/example/notificaciones/MainActivity.kt
@@ -0,0 +1,199 @@
+package com.example.notificaciones
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.notificaciones.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // PASO CERO hacer lo del binding: ésto y el crearObjetosDelXml
+    private lateinit var binding: ActivityMainBinding
+
+    // PRIMER PASO creamos una variable: identificador del canal
+    // que vamos a utilizar para las notificaciones
+    private val CHANNEL_ID: String = "123"
+
+    private var PETICION_PERMISOS: Int = 456 // ID para los permisos que se pidan
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crearObjetosDelXml()
+
+        // SEGUNDO PASO
+        // nada más ejecutarse el onCreate, tiene que crearnos
+        // ese canal de notificaciones que nos hemos declarado
+        // en la variable un poco más arriba
+        createNotificationChannel()
+    }
+
+
+    private fun crearObjetosDelXml() {
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+    // TERCER PASO
+    // Creamos la función que genera el canal de
+    // notificaciones que vamos a utilizar
+    private fun createNotificationChannel() {
+        // Comprobamos la versión del SDK, porque
+        // hasta cierta API no existían los canales
+        // de notificaciones
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Al canal tenemos que darle un nombre, un texto descriptivo,
+            // y un grado de importancia para las notificacionees
+            val name = "Mi aplicación"
+            val descriptionText = "Canal para mis notificaciones"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            // CUARTO PASO: DECLARACIÓN DEL CANAL
+            // Creamos un canal pasando esta información que acabamos de describir
+            // Le pasamos al channel el channelId, el name
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply{
+                description = descriptionText
+            }
+
+
+            // QUINTO PASO
+            // Registrar el canal en el sistema: me declaro un
+            // notificationManager y lo creo pasándole como NotificationChannel
+            // el que me he creado anteriormente
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+
+    }
+
+
+    // SEXTO PASO
+    // Crear método que permita lanzar las notificaciones.
+    // Se lo va a llamar clickando el botón: no puede ser privado, y
+    // por parámetro tiene que recibir una View, que será el propio botón
+    fun lanzarNotificaciones(view: View) {
+
+        // Queremos que se haga algo cuando se pulse en la notificación; creamos
+        // un intent, una intención
+
+        // PendingIntent permite lanzar algo al sistema a alguien, al margen de que
+        // la aplicación que ha hecho el lanzamiento ya no esté en ejecución; se
+        // queda ahí aunque hayas cerrado la aplicación
+        val contentIntent = PendingIntent.getActivity(
+            this,
+            0,
+            // FLAG_IMMUTABLE hace que el contenido de PendingIntent no pueda ser modificado
+            // Puede haber casos en que lance un PendingIntent varias veces y se pueda
+            // modificar; no es este caso.
+            Intent(this, MainActivity::class.java), PendingIntent.FLAG_IMMUTABLE)
+
+        // Ahora hay que decirle a la app que ejecute ese PendingIntent cuando
+        // pulsemos en la notificación: línea de .setContentIntent(contentIntent)
+        // Con ello hago que, al pulsar en la notificación desde fuera de la app,
+        // me devuelva a ella
+
+
+
+
+
+        // Para lanzar la notificación, es necesario crear un builder de
+        // notificaciones asociado a nuestro canal
+        // this: el contexto en el cual estamos
+        // CHANNEL_ID: el canal creado anteriormente
+
+        when (view) {
+
+            binding.button1 -> {
+                var notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                    // Ésto ya espara toquetear propiedades
+                    // Icono de advertencia:
+                    .setSmallIcon(android.R.drawable.stat_sys_warning)
+                    // Un título:
+                    .setContentTitle("Aviso")
+                    // Se recomienda mirar en el API todo lo que se puede hacer con las notificaciones
+                    // Un texto:
+                    .setContentText("Ésto es un aviso de notificación")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(contentIntent)
+
+                // Mostrar la notificación
+                // Pasamos un ID. No el del canal, sino el de la notificación; cada
+                // notificación tiene que tener un ID diferente
+                with(NotificationManagerCompat.from(this)){
+                    notify(1111, notificationBuilder.build())
+                }
+            }
+
+            binding.button2 -> {
+                var notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                    // Ésto ya espara toquetear propiedades
+                    // Icono de advertencia:
+                    .setSmallIcon(android.R.drawable.stat_sys_warning)
+                    // Un título:
+                    .setContentTitle("Aviso")
+                    // Se recomienda mirar en el API todo lo que se puede hacer con las notificaciones
+                    // Un texto:
+                    .setContentText("FUNCIONA CHAVALES")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(contentIntent)
+
+                // Mostrar la notificación
+                // Pasamos un ID. No el del canal, sino el de la notificación; cada
+                // notificación tiene que tener un ID diferente
+                with(NotificationManagerCompat.from(this)){
+                    notify(2222, notificationBuilder.build())
+                }
+            }
+        }
+
+
+        // TODO ESTE BLOQUE LO VAMOS A METER DENTRO DE UN IF QUE COMPRUEBE SI EL
+        // USUARIO YA DENEGÓ PERMISOS, PARA QUE NO VUELVA A PREGUNTAR MÁS Y EN
+        // RESUMEN NO SEA UN PESAO DE LA MIERDA
+
+
+        // Proceso de los permisos: verifica si ya se le preguntó al usuario por
+        // los permisos, los denegó y decidió que no se le volviese a preguntar
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.POST_NOTIFICATIONS)) {
+            Toast.makeText(this, "Tienes que darme permisos en ajustes", Toast.LENGTH_SHORT).show()
+        } else {
+            // Verificamos si se ha concedido el permiso
+
+            // Hago un check. Si no está concedido el permiso, en
+            // ese momento le pido permiso al usuario (estas líneas
+            // se han puesto auto al hacer alt+shift+intro)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // Se solicitan permisos al usuario en caso de que no los haya dado
+                // Solicitamos los permisos
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.POST_NOTIFICATIONS
+                    ), PETICION_PERMISOS)
+            }
+        }
+
+
+
+
+    }
+}
