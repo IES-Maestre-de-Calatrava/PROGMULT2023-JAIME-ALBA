8fe09157e67449fd39caf860352272a9752ec570
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2023-10-29 16:29:25 +0100

    29/10/2023
    Implementación del menú contextual funcional. Comienzo del rediseño para separar el diario de caza en dos activities diferentes: una vista general y la adición/visualización-modificación-eliminación de notas.

--- MonsterHunterFinder/app/src/main/java/com/example/monsterhunterfinder/activity_diario.kt
+++ MonsterHunterFinder/app/src/main/java/com/example/monsterhunterfinder/activity_diario.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -20,6 +21,8 @@ class activity_diario : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        registerForContextMenu(binding.textoArmaUtilizada)
     }
 
     private fun crearObjetosDelXml() {
@@ -62,21 +65,109 @@ class activity_diario : AppCompatActivity() {
     fun mostrarOpciones(view: View) {
         val builder = AlertDialog.Builder(this)
 
-        builder.setTitle("VACIAR CAMPOS")
-            .setMessage("¿Quieres vaciar los campos?")
+        builder.setTitle(R.string.cuidado)
+            .setMessage(R.string.vaciarCamposPregunta)
             .setCancelable(false) // El usuario no puede salirse clickando fuera
             .setPositiveButton(
-                "¡Seguro!",
+                R.string.vaciarCamposSi,
                 DialogInterface.OnClickListener{
                     dialog, id -> vaciarCampos()
                 }
             )
             .setNegativeButton(
-                "Mejor no.",
+                R.string.vaciarCamposNo,
                 DialogInterface.OnClickListener{
                     dialog, id -> dialog.cancel()
                 }
             )
             .show()
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        when (v) {
+            binding.textoArmaUtilizada -> menuInflater.inflate(R.menu.menu_armas, menu)
+        }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menuGranEspada -> {
+                binding.textoArmaUtilizada.setText(R.string.granEspada)
+                true
+            }
+
+            R.id.menuEspadaLarga -> {
+                binding.textoArmaUtilizada.setText(R.string.espadaLarga)
+                true
+            }
+
+            R.id.menuEspadaYEscudo -> {
+                binding.textoArmaUtilizada.setText(R.string.espadaYEscudo)
+                true
+            }
+
+            R.id.menuEspadasDobles -> {
+                binding.textoArmaUtilizada.setText(R.string.espadasDobles)
+                true
+            }
+
+            R.id.menuLanza -> {
+                binding.textoArmaUtilizada.setText(R.string.lanza)
+                true
+            }
+
+            R.id.menuLanzaPistola -> {
+                binding.textoArmaUtilizada.setText(R.string.lanzaPistola)
+                true
+            }
+
+            R.id.menuMartillo -> {
+                binding.textoArmaUtilizada.setText(R.string.martillo)
+                true
+            }
+
+            R.id.menuCornamusa -> {
+                binding.textoArmaUtilizada.setText(R.string.cornamusa)
+                true
+            }
+
+            R.id.menuHachaEspada -> {
+                binding.textoArmaUtilizada.setText(R.string.hachaEspada)
+                true
+            }
+
+            R.id.menuHachaCargada -> {
+                binding.textoArmaUtilizada.setText(R.string.hachaCargada)
+                true
+            }
+
+            R.id.menuGlaiveInsecto -> {
+                binding.textoArmaUtilizada.setText(R.string.glaiveInsecto)
+                true
+            }
+
+            R.id.menuBallestaLigera -> {
+                binding.textoArmaUtilizada.setText(R.string.ballestaLigera)
+                true
+            }
+
+            R.id.menuBallestaPesada -> {
+                binding.textoArmaUtilizada.setText(R.string.ballestaPesada)
+                true
+            }
+
+            R.id.menuArco -> {
+                binding.textoArmaUtilizada.setText(R.string.arco)
+                true
+            }
+
+            else -> super.onContextItemSelected(item)
+        }
     }
 }
