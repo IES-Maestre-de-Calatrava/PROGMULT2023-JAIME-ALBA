af680b76ae2c74b10f938d0a3c6bfae1647d8973
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2023-12-04 10:41:01 +0100

    04/12/2023
    Proyectos de estados de audio controlados según estados de activity y de reproducción de audio con seekBar.

---
+++ EstadosMediaPlayer_v34/app/src/main/java/com/break4learning/estadosmediaplayer_v34/MainActivity.kt
@@ -0,0 +1,159 @@
+package com.break4learning.estadosmediaplayer_v34
+
+import android.media.MediaPlayer
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.break4learning.estadosmediaplayer_v34.databinding.ActivityMainBinding
+import java.io.IOException
+
+// Los métodos aquí mostrados pueden usarse para guardar información en general
+
+class MainActivity : AppCompatActivity() {
+    // Programa que mediante un MediaPlayer guarda los distintos estados
+    // por los que pasa nuestra aplicación.
+    // También guarda el estado en el que estaba un audio para reproducir por
+    // el mismo sitio por el que se había quedado.
+    // Los mensajes a transmitir se van a pasar por el logcat; vamos a poder
+    // ver los distintos estados.
+    /**
+     * Binding
+     */
+    private lateinit var binding: ActivityMainBinding
+
+    // Variables MediaPlayer y para guardar la posición por la que va el audio
+    var mediaPlayer: MediaPlayer? = null
+    var pos = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crearObjetosDelXml()
+
+        // Mensaje para indicar que estamos en el onCreate
+        Log.d("MultimediaLog", "En el onCreate")
+
+        // Creamos el objeto como instancia de la clase importada
+        mediaPlayer = MediaPlayer()
+
+        // Para cargar en el mediaPlayer un archivo de audio:
+        try {
+            mediaPlayer!!.setDataSource(
+                this,
+                // Contexto y uri parse de la ruta del archivo de audio
+                Uri.parse("android.resource://"+packageName+"/"+R.raw.bailar)
+            )
+            // Y le decimos que se prepare para reproducirlo
+            mediaPlayer!!.prepare() // Este prepare suele dejar bloqueado el hilo principal mientras
+            // carga el audio; con audios en local no pasa nada, pero con audios que tenga que descargar
+            // tengo que usar un prepare asíncrono para que reproduzca cuando termine de descargar
+
+            // Si uso el MediaPlayer.create(), no puedo crear asíncronos
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    // El método que se ejecuta justo tras el onCreate es el onResume; en él le vamos a decir
+    // que reproduzca el audio
+    override fun onResume(){
+        super.onResume()
+
+        // Le vamos a decir que nos diga que se encuentra en el onResume y por qué posición
+        // del audio va
+        // Antes de darle al start tenemos que asegurarnos de que el mediaPlayer no sea null,
+        // porque si lo es produce una excepción
+        Log.d("MultimediaLog", "En el onResume");
+        Log.d("MultimediaLog", "Valor de pos = $pos");
+
+        if (mediaPlayer != null) {
+            //mediaPlayer!!.seekTo(pos)
+            mediaPlayer!!.start()
+        }
+    }
+
+    // Para que libere recursos cuando se termine: método onDestroy, método que se ejecuta
+    // cuando se destruye una activity
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Al girarlo, se destruye, se vuelve a crear y el audio
+        // se reproduce desde el principio
+
+        // Si la pongo en segundo plano y luego vuelvo, lo que se
+        // ejecuta es el onResume
+        Log.d("MultimediaLog", "En el onDestroy")
+        if (mediaPlayer != null) {
+            mediaPlayer!!.release()
+            mediaPlayer = null
+        }
+    }
+
+
+    // Cuando se detiene la app, se ejecuta el método onPause
+    // Se ejecuta tanto al girar el móvil como al poner la app en segundo plano
+    override fun onPause() {
+        super.onPause()
+        Log.d("MultimediaLog", "En el onPause")
+
+        // Cosas que podríamos hacer: que, cuando la app pase a segundo plano,
+        // el audio se pause
+
+
+        // Primero nos guardamos el punto por el que va reproduciendo el audio,
+        // y le decimos al mediaPlayer que pause
+        if (mediaPlayer != null) {
+            pos = mediaPlayer!!.currentPosition
+            mediaPlayer!!.pause()
+        }
+
+        // Hay un método que se ejecuta en el que voy a guardar la posición por
+        // la que va el reproductor (no tengo que decirle dónde lo tiene que guardar,
+        // se ejecuta solo
+        // Es éste:
+    }
+
+    override fun onSaveInstanceState(bundle: Bundle) {
+        super.onSaveInstanceState(bundle)
+
+        Log.d("MultimediaLog", "En el onSaveInstanceState")
+
+        // Aquí guardamos la posición por la que va el reproductor
+        if (mediaPlayer != null) {
+            // Para guardar una variable en el bundle, en el paquete:
+            bundle.putInt("posicion", pos)
+        }
+
+        // Tras el onPause, pasa por este método y hace lo que nosotros le pongamos
+        // Es como un "guardar información"; esta información almacenada se recupera
+        // con un método que se ejecuta tras el onResume
+    }
+
+    override fun onRestoreInstanceState(bundle: Bundle) {
+        super.onRestoreInstanceState(bundle!!) // Por si el bundle llega vacío
+
+        Log.d("MultimediaLog", "En el onRestoreInstanceState")
+
+        // Comprobamos si el bundle trae datos y si el mediaPlayer NO es null
+        if (bundle != null && mediaPlayer != null) {
+            pos = bundle.getInt("posicion")
+        }
+    }
+
+    // Tras mirar el Logcat: el onSaveInstanceState se ejecuta DESPUÉS DEL ONCREATE
+    // El onSaveInstanceState se ejecuta ANTES DEL ONDESTROY
+    // Se guarda info antes del ondestroy, y tras un nuevo oncreate se restaura
+    // Igualmente también se guarda info en el onpause que se restaura en el onresume
+    // Más cosas en el PDF de reproducir multimedia, en el classroom de R3 Multimedia
+
+
+    /**
+     * Crear objetos del xml
+     *
+     */
+    private fun crearObjetosDelXml(){
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ ReproductorAudioBotonesSimple_v34/app/src/main/java/com/break4learning/reproductoraudiobotonessimple_v34/MainActivity.kt
@@ -0,0 +1,178 @@
+package com.break4learning.reproductoraudiobotonessimple_v34
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.SeekBar
+import com.break4learning.reproductoraudiobotonessimple_v34.databinding.ActivityMainBinding
+
+
+// Primero extendemos de MediaPlayer; cuando nos pregunte si queremos implementar
+// los métodos, le decimos que sí
+class MainActivity : AppCompatActivity(), MediaPlayer.OnCompletionListener {
+    /**
+     * Binding
+     */
+    private lateinit var binding: ActivityMainBinding
+
+    // Preparamos el mediaPlayer
+    private var mediaPlayer: MediaPlayer? = null
+    // A la mutable list le vamos a pasar los sonidos que queremos reproducir
+    private var sonidoActual = mutableListOf(R.raw.bailar)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crearObjetosDelXml()
+
+        // Hasta que no se empiece a reproducir el audio, el usuario no podrá tocar la seekbar
+        binding.seekBar.isEnabled = false
+
+
+        // Reproducción de sonidos:
+        // luego habrá que tocarlo para decirle que pase al sigueinte audio
+        controlSonido(sonidoActual[0])
+    }
+
+    private fun controlSonido(id: Int) {
+        // Me creo listeners para cada botón
+        binding.playButton.setOnClickListener {
+            iniciarReproduccion(id)
+        }
+
+
+        binding.pauseButton.setOnClickListener {
+            if (mediaPlayer != null) {
+                // Si estaba ejecutando, pausa. Si no, reproduce.
+                if (mediaPlayer!!.isPlaying) {
+                    mediaPlayer!!.pause()
+                } else {
+                    mediaPlayer!!.start()
+                }
+            }
+        }
+
+
+        binding.stopButton.setOnClickListener {
+            // Detiene el reproductor, no el audio.
+            // Funcionará con el onCompletion, cuando la reproducción se acabe.
+            pararReproductor()
+        }
+
+
+        // Voy a controlar también que el usuario toque la barra para que la canción avance
+        // hasta el punto requerido
+        // Darle a que sí importe los tres métodos
+        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    // Se pasa el progreso requerido en la barra y el mediaPlayer
+                    // se sitúa en donde corresponda; ESO SIEMPRE Y CUANDO HAGA QUE
+                    // EL TAMAÑO DE LA SEEKBAR COINCIDA CON LA DURACIÓN DEL AUDIO
+                    mediaPlayer!!.seekTo(progress)
+                }
+            }
+
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+    }
+
+    private fun iniciarReproduccion(id: Int) {
+        if (mediaPlayer == null) {
+            // Tengo que hacer un proceso de creación cuando el mediaPlayer sea nulo.
+            // Si lo hago sin que sea nulo, me va a dar una excepción.
+            mediaPlayer = MediaPlayer.create(this, id)
+            mediaPlayer!!.setOnCompletionListener(this)
+
+            // También inicializamos la seekBar; lo vamos a hacer con un hilo
+            inicializarSeekBar()
+
+            mediaPlayer!!.start()
+
+            // Y aquí jugamos con habilitar o deshabilitar botones para evitar errores.
+            // Si hay reproducción activa, deshabilito el botón play. A los otros dos sí
+            // les puedo dar.
+            binding.playButton.isEnabled = false
+            binding.stopButton.isEnabled = true
+            binding.pauseButton.isEnabled = true
+            // Y habilitamos la seekbar
+            binding.seekBar.isEnabled = true
+        }
+    }
+
+    private fun inicializarSeekBar() {
+        // Usamos un handler: lo usamos para enviar mensajes y objetos a una cola que puede tener
+        // un thread; un hilo de ejecución ejecuta una tarea y se destruye, pero aquí no: le paso una
+        // cola de tareas y al acabar una pasa a la siguiente
+
+        // Antes de nada le pongo tamaño a la seekBar
+        binding.seekBar.max = mediaPlayer!!.duration
+
+        // Coger el import de android
+        val handler = Handler(Looper.getMainLooper())
+
+        // Le paso lo que quiero que ejecute en ese hilo
+        // Dos parámetros: lo que quiero que ejecute en segundo plano Y cuándo quiero que
+        // empiece a ejecutarse este hilo
+        handler.postDelayed(
+            object: Runnable {
+                override fun run() {
+                    try {
+                        // La seekBar va a ir avanzando cada segundo
+                        binding.seekBar.progress = mediaPlayer!!.currentPosition
+                        handler.postDelayed(this, 1000) // Reejecutar el run cada mil milisegundos
+                    } catch (e: Exception) {
+
+                    }
+                }
+            }
+        , 0)
+    }
+
+    private fun pararReproductor() {
+        // Comprobamos que mediaPlayer está funcionando
+        if (mediaPlayer != null) {
+            mediaPlayer!!.stop()
+            // Si da problemas al detenerlo: mediaPlayer.reset()
+            mediaPlayer!!.release() // Para liberar recursos
+            mediaPlayer = null
+
+            // Y rehago el juego de botones habilitados
+            binding.playButton.isEnabled = true
+            binding.stopButton.isEnabled = false
+            binding.pauseButton.isEnabled = false
+            // Deshabilitamos la seekBar
+            binding.seekBar.isEnabled = false
+        }
+    }
+
+    /**
+     * Crear objetos del xml
+     *
+     */
+    private fun crearObjetosDelXml(){
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCompletion(mp: MediaPlayer?) {
+        // Este método se ejecuta cuando termina de reproducirse el audio.
+        // En el método pararReproductor() vamos a hacer todo lo relativo a la liberación de recursos.
+        pararReproductor()
+    }
+
+
+    // TAREA: si lo giro, se pierde todo el control sobre el audio. Solucionarlo.
+    // Y: pasarle una lista de audios, meterle dos botones y que vaya yendo para adelante y para
+    // atrás en los archivos.
+}
