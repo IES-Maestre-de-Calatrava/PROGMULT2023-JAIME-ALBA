cdc92c9df89dc9bc8be267eab4bad172e79f2126
jaimealba <jaimealba@outlook.es>
2024-01-11 22:39:03 +0100

    11/01/2024 22:39
    Preview del vídeo funcional en la activity de añadir. Implementada grabación de vídeo y adición a app. Implementado hacer fotos y adición a galería y app.

--- MonsterHunterFinder/app/src/main/java/com/example/monsterhunterfinder/activity_perfil_anadir.kt
+++ MonsterHunterFinder/app/src/main/java/com/example/monsterhunterfinder/activity_perfil_anadir.kt
@@ -1,17 +1,28 @@
 package com.example.monsterhunterfinder
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.net.Uri
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.View
 import android.widget.VideoView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
 import androidx.core.view.isVisible
 import com.example.monsterhunterfinder.databinding.ActivityPerfilAnadirBinding
 import com.google.firebase.storage.FirebaseStorage
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
 
 /**
  * Activity dedicada a la adición de imágenes a la
@@ -30,11 +41,14 @@ class activity_perfil_anadir : AppCompatActivity() {
     // del almacenamiento del teléfono, y variables para controlar que
     // estas acciones se hayan realizado
     lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    var hayVideo = false
+    var hayFoto = false
+
     var cogeVideo = false
     var cogeFoto = false
 
     // Variable para el videoView
-    lateinit var mVideoView: VideoView
+    private var mVideoView: VideoView? = null
 
     // Uri del vídeo escogido (de la galería)
     lateinit var uriGaleriaVideo: Uri
@@ -45,14 +59,58 @@ class activity_perfil_anadir : AppCompatActivity() {
     // Declaración del acceso al almacenamiento de Firebase
     private val storage = FirebaseStorage.getInstance()
     private val videoStorage = storage.reference
-    // Variables para controlar si hay vídeo y foto
-    var hayVideo = false
-    var hayFoto = false
 
+    // Variables para los permisos de grabar vídeos y hacer fotos
+    private val PETICION_PERMISO_GRABACION = 0
+    private val PETICION_PERMISO_CAMARA = 1
+
+    // Variables para tomar una foto, incluirla en la galería y usarla en la aplicación
+    private var fromCamera = false
+    private lateinit var fotoPath: String
+    private val CAPTURA_IMAGEN_GUARDAR_GALERIA = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crearObjetosDelXml()
+
+
+        // Petición de permisos para grabar vídeo
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO
+                ),
+                PETICION_PERMISO_GRABACION
+            )
+        }
+
+
+        // Petición de permisos para hacer fotos y añadirlas a la galería
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ||
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ),
+                PETICION_PERMISO_CAMARA
+            )
+        }
+
+
+
+        // Inicialmente, los botones para controlar vídeo están deshabilitados
+        binding.botonPlayAnadir.isEnabled = false
+        binding.botonPauseAnadir.isEnabled = false
+        binding.botonStopAnadir.isEnabled = false
+        binding.botonRetrAnadir.isEnabled = false
+        binding.botonAvanAnadir.isEnabled = false
 
         // Inicialmente, el botón de confirmar es invisible.
         // Se hará visible solamente cuando se hayan seleccionado un
@@ -70,10 +128,24 @@ class activity_perfil_anadir : AppCompatActivity() {
             subirVideo()
         }
 
+        // Listener para el botón de grabar vídeo
+        binding.botonGrabarVideo.setOnClickListener{
+            comenzarGrabacion()
+            binding.botonGaleriaVideo.visibility = View.INVISIBLE
+        }
+
         // Listener para el botón de añadir vídeo desde la galería
         binding.botonGaleriaVideo.setOnClickListener {
             SeleccionarVideoDeGaleria()
             binding.botonGrabarVideo.visibility = View.INVISIBLE
+        }
+
+        // Listener para el botón de hacer foto y añadirla a la galería
+        binding.botonHacerFoto.setOnClickListener{
+            if (packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+                dispatchTakePictureIntent(true, CAPTURA_IMAGEN_GUARDAR_GALERIA)
+            }
+            binding.botonGaleriaFoto.visibility = View.INVISIBLE
         }
 
         // Listener para el botón de añadir foto desde la galería
@@ -93,23 +165,33 @@ class activity_perfil_anadir : AppCompatActivity() {
 
                 // Caso de que se haya seleccionado un vídeo
                 if (cogeVideo) {
+                    hayVideo = true
+                    cogeVideo = false
                     actualizarEstadoBoton()
-
-                    mVideoView = binding.videoViewAnadir
 
                     // El vídeo escogido de la galería se muestra en el videoView,
                     // para que el usuario pueda ver una previsualización de lo que
                     // va a subir a la galería de su perfil
-                    mVideoView.setVideoURI(data.data)
                     uriGaleriaVideo = data.data!!
-                    hayVideo = true
+
+                    // Método para controlar la reproducción de vídeo
+                    controlVideo()
 
                 // Caso de que se haya seleccionado una foto
                 } else if (cogeFoto) {
-                    actualizarEstadoBoton()
-
-                    uriGaleriaFoto = data.data!!
                     hayFoto = true
+                    cogeFoto = false
+
+                    if (fromCamera) {
+                        if (resultado.resultCode == RESULT_OK) {
+                            uriGaleriaFoto = Uri.fromFile(File(fotoPath))
+                            refreshGallery()
+                        }
+                    } else {
+                        uriGaleriaFoto = data.data!!
+                    }
+
+                    actualizarEstadoBoton()
                 }
 
             }
@@ -117,11 +199,237 @@ class activity_perfil_anadir : AppCompatActivity() {
     }
 
     /**
+     * Método que, en caso de que el dispositivo disponga de una característica capaz de
+     * atender una llamada de la cámara, realiza la llamada para permitir hacer una foto
+     * e insertarla en la galería
+     */
+    private fun dispatchTakePictureIntent(galeria: Boolean, code: Int) {
+        // En los parámetros indicamos si queremos o no que lo guarde en la galería
+        val intent = Intent()
+        intent.action = MediaStore.ACTION_IMAGE_CAPTURE
+
+        if (intent.resolveActivity(packageManager) != null) {
+            // Llamamos a un método que crea una imagen en la galería
+            // Se ejecuta si galería está a true, y no en caso contrario
+            val photoFile = createImageFile(galeria)
+
+            // Si el método devuelve una ruta
+            if (photoFile != null) {
+                fotoPath = photoFile!!.absolutePath
+
+                // Le pasamos el contexto, el nombre del paquete (authorities del manifest) y la ruta al archivo de la galería
+                val photoURI = FileProvider.getUriForFile(this, "com.example.monsterhunterfinder", photoFile)
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+
+                if (intent.resolveActivity(packageManager) != null) {
+                    fromCamera = true
+                    cogeFoto = true
+                    when (code) {
+                        CAPTURA_IMAGEN_GUARDAR_GALERIA -> activityResultLauncher.launch(intent)
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Método que devuelve un archivo, que será el archivo en el que se almacene la foto
+     *
+     * @param galeria: variable que indica si el archivo se insertará o no en la galería
+     */
+    private fun createImageFile(galeria: Boolean): File? {
+        var image: File? = null
+        // Crea nombres de archivos usando la fecha y la hora del sistema (coger los imports de java)
+        val timeStamp = SimpleDateFormat("yyyMMdd_HHmmss").format(Date())
+        val imageFileName = "IMG_" + timeStamp + "_"
+
+        // Una cosa va a ser el nombre del archivo y otra la carpeta en que lo almacenamos
+        // (galería o almacenamiento interno privado de la aplicación)
+        var storageDir: File? = null
+
+        storageDir =
+            if (galeria) {
+                // La parte que nos interesa; que lo coja y lo meta en la galería
+                File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString()+"/Camera/"
+                )
+            } else {
+                // No hace nada pero lo dejamos así, el método da problemas
+                getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            }
+
+        // Si el directorio no existe, lo creamos
+        if (!storageDir!!.exists()) {
+            storageDir.mkdirs()
+        }
+
+        // Y creamos el archivo; lo creamos temporal y lo vamosa devolver para que
+        // meta algo en él
+
+        // Le pasamos el imageFileName, nombre del fichero
+        // Le pasamos la extensión
+        // Le pasamos el directorio donde queremos meterlo
+        image = File.createTempFile(
+            imageFileName, ".jpeg",storageDir
+        )
+
+        return image
+    }
+
+    /**
+     * Método que refresca la galería, para que sean
+     * visibles los cambios hechos en su contenido
+     */
+    private fun refreshGallery() {
+        val f = File(fotoPath)
+        MediaScannerConnection.scanFile(this, arrayOf(f.toString()), null, null)
+    }
+
+    /**
+     * Método que inicia la grabación de vídeo para reproducirlo en la aplicación
+     * y posteriormente subirlo al storage de la Firebase y asignarlo a un item
+     * de la galería del perfil del usuario.
+     */
+    private fun comenzarGrabacion() {
+        val intent = Intent()
+
+        intent.action = MediaStore.ACTION_VIDEO_CAPTURE
+
+        if (intent.resolveActivity(packageManager) != null) {
+            activityResultLauncher.launch(intent)
+            cogeVideo = true
+        }
+    }
+
+
+    /**
+     * Método que permite, una vez seleccionado o grabado un vídeo, controlar
+     * la reproducción del mismo en la VideoView
+     */
+    private fun controlVideo() {
+        if (hayVideo) {
+            // Se habilita el botón de Play para poder iniciar la reproducción
+            binding.botonPlayAnadir.isEnabled = true
+
+
+            // Se establecen listeners para cada botón
+            binding.botonPlayAnadir.setOnClickListener{
+                cargarMultimedia()
+            }
+
+            binding.botonPauseAnadir.setOnClickListener{
+                if (mVideoView != null) {
+                    if (mVideoView!!.isPlaying) {
+                        mVideoView!!.pause()
+                    } else {
+                        mVideoView!!.start()
+                    }
+                }
+            }
+
+            binding.botonStopAnadir.setOnClickListener{
+                pararReproduccion()
+            }
+
+            binding.botonRetrAnadir.setOnClickListener{
+                retrocederReproduccion(10000)
+            }
+
+            binding.botonAvanAnadir.setOnClickListener{
+                avanzarReproduccion(10000)
+            }
+        }
+    }
+
+    /**
+     * Método que verifica si el reproductor de vídeo con el que se trabaja
+     * es nulo, lo crea en caso afirmativo y, en la propia creación, carga
+     * desde el almacenamiento de Firebase el vídeo que va asociado a la
+     * imagen en la que se ha pulsado en la RecyclerView del perfil de usuario.
+     */
+    private fun cargarMultimedia() {
+
+        if (mVideoView == null) {
+            // Ligamos el reproductor a la VideoView del layout
+            mVideoView = binding.videoViewAnadir
+            mVideoView!!.setOnCompletionListener { pararReproduccion() }
+            // El reproductor de vídeo se creará usando como URI la del vídeo de la galería
+            mVideoView!!.setVideoURI(uriGaleriaVideo)
+        }
+
+        // Tras el proceso de creación, la reproducción se inicia
+        mVideoView!!.setBackgroundColor(Color.TRANSPARENT)
+        mVideoView!!.start()
+
+        // Y aquí jugamos con habilitar o deshabilitar botones para evitar errores.
+        // Si hay reproducción activa, deshabilito el botón play. A los otros sí
+        // se les puede dar.
+        binding.botonPlayAnadir.isEnabled = false
+        binding.botonStopAnadir.isEnabled = true
+        binding.botonPauseAnadir.isEnabled = true
+        binding.botonRetrAnadir.isEnabled = true
+        binding.botonAvanAnadir.isEnabled = true
+    }
+
+    /**
+     * Función que atrasa la reproducción, restando tiempo
+     * a la currentPosition de la VideoView.
+     * El coerceIn se usa para vigilar que la posición final
+     * esté dentro de un rango válido del tiempo de ejecución.
+     *
+     * @param millis: Número entero que equivale al momento actual de la reproducción
+     */
+    private fun retrocederReproduccion(millis: Int) {
+        if (mVideoView != null) {
+            val newPosition = mVideoView!!.currentPosition - millis
+            mVideoView!!.seekTo(newPosition.coerceIn(0, mVideoView!!.duration))
+        }
+    }
+
+    /**
+     * Función que adelanta la reproducción, sumando tiempo
+     * a la currentPosition de la VideoView.
+     * El coerceIn se usa para vigilar que la posición final
+     * esté dentro de un rango válido del tiempo de ejecución.
+     *
+     * @param millis: Número entero que equivale al momento actual de la reproducción
+     */
+    private fun avanzarReproduccion(millis: Int) {
+        if (mVideoView != null) {
+            val newPosition = mVideoView!!.currentPosition + millis
+            mVideoView!!.seekTo(newPosition.coerceIn(0, mVideoView!!.duration))
+        }
+    }
+
+    /**
+     * Método empleado para detener la reproducción cuando se presiona el
+     * botón de Stop o cuando la reproducción termina. La VideoView se
+     * iguala a null para destruir la instancia que se esté utilizando.
+     */
+    private fun pararReproduccion() {
+        if (mVideoView != null) {
+            mVideoView!!.pause()
+
+            mVideoView!!.setBackgroundColor(Color.BLACK)
+            mVideoView = null
+
+            binding.botonPlayAnadir.isEnabled = true
+            binding.botonStopAnadir.isEnabled = false
+            binding.botonPauseAnadir.isEnabled = false
+            binding.botonRetrAnadir.isEnabled = false
+            binding.botonAvanAnadir.isEnabled = false
+        }
+    }
+
+
+
+    /**
      * Método que se usará para habilitar o deshabilitar el botón de confirmar;
      * únicamente se habilitará cuando se hayan seleccionado un vídeo y una foto
      */
     private fun actualizarEstadoBoton() {
-        if (cogeVideo && cogeFoto) {
+        if (hayVideo && hayFoto) {
             binding.botonConfirmarAnadir.visibility = View.VISIBLE
         } else {
             binding.botonConfirmarAnadir.visibility = View.INVISIBLE
